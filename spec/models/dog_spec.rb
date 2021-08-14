97d1bd7a91da388963b8121539c6cb12fabdcd42
thisisdylandev <dylan@ctrlaltdylan.com>
2021-08-13 19:24:13 -0500

    test: add tests for dog/user association
    installed shoulda for better testing DX, added tests for dogs belonging to users

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,7 +1,19 @@
-# require "rails_helper"
+require "rails_helper"
 
-# RSpec.describe Dog, type: :model do
-#   it "is valid with valid attributes" do
-#     expect(new_application).to be_valid
-#   end
-# end
+RSpec.describe Dog, type: :model do
+  it "is valid with valid attributes" do
+    newDog = FactoryBot.build(:dog)
+    expect(newDog).to be_valid
+  end
+
+  it "should belong to a User" do
+    newDog = FactoryBot.build(:dog)
+    expect(newDog).to belong_to(:user)
+  end
+
+  it "is not valid without a user" do
+    no_user = FactoryBot.build(:dog)
+    no_user.user = nil
+    expect(no_user).to_not be_valid
+  end
+end
